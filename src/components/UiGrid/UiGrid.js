ed3622a633e0f7097450c40efea71629b127763a
GyozaGuy <gyozaguy@gmail.com>
2022-03-16 10:04:54 -0600

    Updated UiGrid

--- src/components/UiGrid/UiGrid.js
+++ src/components/UiGrid/UiGrid.js
@@ -5,18 +5,6 @@ class UiGrid extends Component {
   static styles = css`
     ${styles}
   `;
-
-  connectedCallback() {
-    let gapValue = this.getAttribute('gap');
-
-    if (gapValue) {
-      if (/\d$/.test(gapValue)) {
-        gapValue += 'px';
-      }
-
-      this.style.setProperty('--gap', gapValue);
-    }
-  }
 }
 
 customElements.define('ui-grid', UiGrid);
