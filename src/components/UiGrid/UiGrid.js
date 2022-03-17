7c8c1ae8ab3cbdf1bbce78f79184ab9ca8bef9c0
GyozaGuy <gyozaguy@gmail.com>
2022-03-16 18:32:26 -0600

    Allow variable columns in UiGrid

--- src/components/UiGrid/UiGrid.js
+++ src/components/UiGrid/UiGrid.js
@@ -5,6 +5,10 @@ class UiGrid extends Component {
   static styles = css`
     ${styles}
   `;
+
+  connected() {
+    this.style.setProperty('--ui-grid-columns', this.getAttribute('columns'));
+  }
 }
 
 customElements.define('ui-grid', UiGrid);
