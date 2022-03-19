70f52ed5e144cdff0c8b975b944581fed930c8c6
GyozaGuy <gyozaguy@gmail.com>
2022-03-19 15:56:07 -0600

    Removed UiHeading

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import './UiCard/UiCard';
 import './UiGrid/UiGrid';
-import './UiHeading/UiHeading';
 import './UiLayoutBand/UiLayoutBand';
 import './UiRow/UiRow';
 import './UiSeparator/UiSeparator';
