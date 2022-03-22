945273593138aa4f910891ae0c1e33756ef60388
GyozaGuy <gyozaguy@gmail.com>
2022-03-21 18:59:24 -0600

    Automatically handle IDs and events

--- src/components/UiDialog/UiDialog.js
+++ src/components/UiDialog/UiDialog.js
@@ -6,18 +6,9 @@ class UiDialog extends Component {
     ${styles}
   `;
 
-  dialog;
-
   connected() {
-    this.dialog = this.shadowRoot.querySelector('dialog');
-    const closeDialogButton = this.dialog.querySelector('#closeDialogButton');
-
     this.dialog.addEventListener('close', () => {
       this.dialog.removeAttribute('opened');
-    });
-
-    closeDialogButton.addEventListener('click', () => {
-      this.dialog.close();
     });
 
     const dialogObserver = new MutationObserver(mutations => {
@@ -33,10 +24,10 @@ class UiDialog extends Component {
 
   render() {
     return html`
-      <dialog>
+      <dialog id="dialog">
         <header>
           <slot name="title"></slot>
-          <button id="closeDialogButton" type="button">❌</button>
+          <button id="closeDialogButton" onclick="close" type="button">❌</button>
         </header>
 
         <div id="content"><slot name="content"></slot></div>
--- src/helpers/Component.js
+++ src/helpers/Component.js
@@ -1,3 +1,5 @@
+import { clearChildren, fhtml } from '.';
+
 export default class extends HTMLElement {
   constructor() {
     super();
@@ -23,14 +25,48 @@ export default class extends HTMLElement {
 
   handleChildrenUpdate() {}
 
+  populateReferences() {
+    function checkAttrs(element) {
+      if (!this) {
+        return;
+      }
+
+      for (const child of element.children) {
+        if (['SLOT', 'STYLE'].includes(child.tagName)) {
+          continue;
+        }
+
+        for (const attr of child.attributes) {
+          const { name, value } = attr;
+
+          if (name === 'id') {
+            this[value] = child;
+          }
+
+          if (name.startsWith('on')) {
+            child.addEventListener(name.substr(2), this[value].bind(this));
+          }
+        }
+
+        checkAttrs.call(this, child);
+      }
+    }
+
+    checkAttrs.call(this, this.shadowRoot);
+  }
+
   render() {
-    return '<slot></slot>';
+    return '<slot id="slotr"></slot>';
   }
 
   update() {
-    this.shadowRoot.innerHTML = `
+    const fragment = fhtml`
       ${this.constructor.styles || ''}
       ${this.render()}
     `;
+
+    clearChildren(this.shadowRoot);
+    this.shadowRoot.appendChild(fragment);
+    this.populateReferences();
   }
 }
